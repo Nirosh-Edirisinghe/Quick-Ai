2420929d6e148384453b7dbf71a2cbf9132992d7
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-07-29 23:14:56 +0530

    create Hero section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
-      <h1>Home</h1>
+      <Hero/>
     </div>
   )
 }
