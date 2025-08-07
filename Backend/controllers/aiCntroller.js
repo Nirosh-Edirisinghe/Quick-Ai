bc83a726746edfffe383f4a6781ac3f5e0a7c918
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-08-08 00:32:12 +0530

    resume review  function

--- Backend/controllers/aiCntroller.js
+++ Backend/controllers/aiCntroller.js
@@ -3,6 +3,8 @@ import sql from "../config/db.js";
 import { clerkClient } from "@clerk/express";
 import axios from "axios";
 import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+import pdf from 'pdf-parse/lib/pdf-parse.js'
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -175,6 +177,49 @@ export const removeImageObject = async (req, res) => {
     await sql`INSERT INTO creations (user_id, promt, content, type) VALUES(${userId}, ${`Remove ${object} from image`}, ${imageUrl}, 'image')`;
 
     res.json({ success: true, imageUrl })
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message })
+
+  }
+}
+
+export const resumeReview = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const resume = req.file;
+    const plan = req.plan;
+
+    if (plan !== 'premium') {
+      return res.json({ success: false, message: 'This feature is only avilable for premium subsriptions' })
+    }
+
+    if(resume.size > 5 * 1024 * 1024){
+      return res.json({success:false, message:'Resume file size exceeds allowed size 5MB.'})
+    }
+    
+    const dataBuffer = fs.readFileSync(resume.path)
+    const pdfData = await pdf (dataBuffer)
+
+    const prompt = `Review  the following resume and provide contructive feedback on its strengths, weakness and area for improvement.Resume Content:\n\n${pdfData.text}`
+
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_tokens: length,
+    });
+
+    const content = response.choices[0].message.content
+
+    await sql`INSERT INTO creations (user_id, promt, content, type) VALUES(${userId},'Review the uploaded resume', ${content}, 'resume-review')`;
+
+    res.json({ success: true, content })
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message })
--- Backend/routes/aiRoutes.js
+++ Backend/routes/aiRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { auth } from '../middlewares/auth.js';
-import { generateArticle, generateBlogTitle, generateImage } from '../controllers/aiCntroller.js';
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from '../controllers/aiCntroller.js';
+import { upload } from '../config/multer.js';
 
 
 const  aiRouter = express.Router();
@@ -10,5 +11,11 @@ aiRouter.post('/generate-article', auth, generateArticle)
 aiRouter.post('/generate-blog-title', auth, generateBlogTitle)
 
 aiRouter.post('/generate-image', auth, generateImage)
+
+aiRouter.post('/remove-image-background',upload.single('image'), auth, removeImageBackground)
+
+aiRouter.post('/remove-image-object',upload.single('image'), auth, removeImageObject)
+
+aiRouter.post('/resume-review',upload.single('resume'), auth, resumeReview)
 
 export default aiRouter
