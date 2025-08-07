ee53825115fbe8bf310989a10be73f43b3a2d8d1
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-08-07 22:10:39 +0530

    creategenerate imae and remove background function

--- Backend/controllers/aiCntroller.js
+++ Backend/controllers/aiCntroller.js
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import sql from "../config/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
+import {v2 as cloudinary} from 'cloudinary'
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -25,7 +27,7 @@ export const generateArticle = async (req, res) => {
           content: prompt,
         },
       ],
-      temperature:0.7,
+      temperature: 0.7,
       max_tokens: length,
     });
 
@@ -33,18 +35,120 @@ export const generateArticle = async (req, res) => {
 
     await sql`INSERT INTO creations (user_id, promt, content, type) VALUES(${userId}, ${prompt}, ${content}, 'article')`;
 
-    if(plan !== 'premium'){
-      await clerkClient.users.updateUserMetadata(userId,{
-        privateMetadata:{
-          free_usage:free_usage + 1
+    if (plan !== 'premium') {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1
         }
       })
     }
-    res.json({success:true, content})
+    res.json({ success: true, content })
   } catch (error) {
     console.log(error.message);
-    res.json({success:false, message:error.message})
-    
+    res.json({ success: false, message: error.message })
+
   }
 }
 
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (plan !== 'premium' && free_usage >= 10) {
+      return res.json({ success: false, message: 'Limit reached. Upgrade to continue.' })
+    }
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content
+
+    await sql`INSERT INTO creations (user_id, promt, content, type) VALUES(${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+    if (plan !== 'premium') {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1
+        }
+      })
+    }
+    res.json({ success: true, content })
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message })
+
+  }
+}
+
+export const generateImage = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt, publish } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (plan !== 'premium') {
+      return res.json({ success: false, message: 'This feature is only avilable for premium subsriptions' })
+    }
+
+    const formData = new FormData()
+    formData.append('prompt', prompt)
+    const {data} =  await axios.post("https://clipdrop-api.co/text-to-image/v1",formData, {
+      headers : {'x-api-key': process.env.CLIPDROP_API_KEY},
+      responseType:'arraybuffer',
+    })
+
+    const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
+
+    const {secure_url} = await cloudinary.uploader.upload(base64Image)
+
+    await sql`INSERT INTO creations (user_id, promt, content, type, publish) VALUES(${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})`;
+
+    res.json({ success: true, secure_url })
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message })
+
+  }
+}
+
+export const removeImageBackground = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { image } = req.file;
+    const plan = req.plan;
+
+    if (plan !== 'premium') {
+      return res.json({ success: false, message: 'This feature is only avilable for premium subsriptions' })
+    }
+
+
+    const {secure_url} = await cloudinary.uploader.upload(image.path,{
+      transformation:[
+        {
+          effect: 'background_rmoval',
+          background_removal: 'remove_the_background'
+        }
+      ]
+    })
+
+    await sql`INSERT INTO creations (user_id, promt, content, type) VALUES(${userId}, 'Remove background from image', ${secure_url}, 'image')`;
+
+    res.json({ success: true, secure_url })
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message })
+
+  }
+}
--- Backend/routes/aiRoutes.js
+++ Backend/routes/aiRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { auth } from '../middlewares/auth.js';
-import { generateArticle } from '../controllers/aiCntroller.js';
+import { generateArticle, generateBlogTitle, generateImage } from '../controllers/aiCntroller.js';
 
 
 const  aiRouter = express.Router();
 
 aiRouter.post('/generate-article', auth, generateArticle)
+
+aiRouter.post('/generate-blog-title', auth, generateBlogTitle)
+
+aiRouter.post('/generate-image', auth, generateImage)
 
 export default aiRouter
